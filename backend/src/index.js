64e3ed9801a00b98fcabeadc4fa9cd14a97394c0
brunoomsantos <Bb030996>
2020-10-21 09:22:06 -0300

    Validação de rotas e MiddleWare

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {uuid} = require('uuidv4')
+const {uuid, isUuid} = require('uuidv4')
 
 const app = express();
 
@@ -8,6 +8,33 @@ app.use(express.json());
 
 
 const projects = [];
+
+function logRequest(request, response, next) {
+    const { method, url } = request;
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+    console.log(logLabel);
+
+    console.time(logLabel);
+
+    next();
+
+    console.timeEnd(logLabel);
+}
+
+function validateProjectId(request, response, next) {
+    const { id } = request.params;
+    
+    if (!isUuid(id)) {
+        return response.status(400).json({ error: 'Invalid project ID.'});
+    }
+
+    return next();
+}
+
+app.use(logRequest);
+app.use('/projects/:id', validateProjectId);
 
 
 app.get('/projects', (request, response) => {
