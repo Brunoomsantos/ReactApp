17f3ecb74e79c07a8c45408ba14aa08527cffbc4
brunoomsantos <brunoomsantos@gmail.com>
2020-10-27 20:14:32 -0300

    Mobile adicionado pra valer dessa vezgit statusgit status!

---
+++ mobile/src/index.js
@@ -0,0 +1,73 @@
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, FlatList, Text, StyleSheet , StatusBar, TouchableOpacity} from 'react-native';
+
+import api from './services/api';
+
+
+export default function App() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+  }, []);
+
+  async function handleAddProject(){
+    const response = await api.post('projects', { 
+        title: `Novo Projeto${Date.now()}`, 
+        owner: 'Bruno Olinto'
+    });
+
+    const project = response.data;
+
+    setProjects([... projects, project]);
+  }
+
+    return (
+        <>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+
+        <SafeAreaView style={styles.container}>
+        <FlatList
+            data={projects}
+            keyExtractor={project => project.id}
+            renderItem={({ item: project }) =>(
+                <Text style={styles.project} key={project.id}> {project.title} </Text>
+            )}
+        />
+
+        <TouchableOpacity activeOpacity={0.6} style={styles.button} onPress={handleAddProject}>
+            <Text style={styles.buttonText}> Adicionar projeto</Text>
+        </TouchableOpacity>
+        </SafeAreaView>
+        </>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor:'#7159c1'
+    },
+    project: {
+        color: '#FFF',
+        fontSize: 30
+    },
+
+    button: {
+        backgroundColor: '#FFF',
+        margin: 20,
+        height: 50,
+        width: 200,
+        borderRadius: 4,
+        alignSelf: 'center'
+    },
+
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        alignSelf: 'center'
+    }
+});
