af207c86aa01425f444a5b6cd56f872933e65418
brunoomsantos <Bb030996>
2020-10-29 13:24:54 -0300

    Typescript course finished

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -2,7 +2,17 @@ import {Request, Response} from 'express';
 import createUser from './services/CreateUsers';
 
 export function helloWorld(request: Request, response: Response) {
-    const user = createUser('Bruno', 'bruno@gmail.com', '123456');
+    const user = createUser({
+        name: 'Bruno', 
+        email: 'bruno@gmail.com',
+        password: '123456',
+        techs: [
+            'Node', 
+            'React', 
+            'React Native', 
+            {title: 'javascript', experience: 100}
+        ],
+    });
 
     return response.json({message: 'Hello World' });
 }
